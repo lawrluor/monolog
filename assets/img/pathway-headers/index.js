6f5d13e14bcda0870b57e2afa52bfa784e999323
jtrieu1998 <jtrieu1998@gmail.com>
2022-10-31 16:14:43 -0500

    Adds images

---
+++ assets/img/pathway-headers/index.js
@@ -0,0 +1,11 @@
+import procrastination_header from "./procrastination.png";
+import anxiety_header from "./anxiety.png";
+import depression_header from "./depression.png";
+import grief_loss_header from "./grief-loss.png";
+
+export {
+  procrastination_header,
+  anxiety_header,
+  depression_header,
+  grief_loss_header,
+}
