4220f1f4661691f7e1314f5faf1e6f61a090a819
Lawrence Luo <luolawrence1@gmail.com>
2022-12-26 16:57:04 -0800

    proof of concept for rendering image tutorials

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -133,6 +133,7 @@ const TabNavigator = (): JSX.Element => {
         listeners={({navigation}) => ({blur: () => navigation.setParams({screen: undefined})})}
       />
 
+      <Tab.Screen name="Rating" component={Rating} options={{ headerShown: false }} />
       <Tab.Screen name="Vistas" component={Vistas} options={{ headerShown: false }} />
       <Tab.Screen name="Pathways" component={Pathways} options={{ headerShown: false }} />
     </Tab.Navigator>
