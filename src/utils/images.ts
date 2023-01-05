1cf49a7fc2e6f0d94b93b20f3948af0b1ef388df
Lawrence Luo <luolawrence1@gmail.com>
2023-01-04 19:34:49 -0800

    added new images for 6.7' device sizes

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,10 +1,32 @@
 import { dimensions } from "../styles";
 
+import { home1large, home2large, home3large, pathwayslarge, recordinglarge, ratinglarge } from "../../assets/img/tutorials/6.7";
 import { home1, home2, home3, pathways, recording, rating} from "../../assets/img/tutorials/6.5";
 import { home1Small, home2Small, home3Small, pathwaysSmall, recordingSmall, ratingSmall} from "../../assets/img/tutorials/5.5";
 
+
 export const getImagesByDeviceSize = (fileBasename: string) => {
-	if (dimensions.height >= 896) {
+	// TODO: Large size: iphone12-14 pro max/plus
+	if (dimensions.height >= 926) {
+		switch(fileBasename) {
+			case 'home1':
+				return home1large;
+			case 'home2':
+				return home2large;
+			case 'home3':
+				return home3large;
+			case 'pathways':
+				return pathwayslarge;
+			case 'recording':
+				return recordinglarge;
+			case 'rating':
+				return ratinglarge;
+			default:
+				return ratinglarge;
+		}
+	}
+	// Medium size
+	if (dimensions.height > 896 && dimensions.height < 926) {
 		switch(fileBasename) {
 			case 'home1':
 				return home1;
